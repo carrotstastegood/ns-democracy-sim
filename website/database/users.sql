92a24fde6ad1dc8c6e29fc95ed883015e03dbf96
carrotstastegood <22matti.kivinen@gmail.com>
2025-08-17 15:15:22 -0400

    Database Changes

--- website/database/users.sql
+++ website/database/users.sql
@@ -6,7 +6,34 @@ create table userInfo (
     username varchar(255)
     email varchar(255)
     pass varchar(255)
-    nation varchar(255)
     totalSeats varchar(255)
 
 );
+
+create table parties (
+
+    uid id
+    party1 varchar(255)
+    tags1 varchar(255)
+    seats1 int
+    party2 varchar(255)
+    tags2 varchar(255)
+    seats2 int
+    party3 varchar(255)
+    tags3 varchar(255)
+    seats3 int
+    party4 varchar(255)
+    tags4 varchar(255)
+    seats4 int
+    party5 varchar(255)
+    tags5 varchar(255)
+    seats5 int
+
+);
+
+create table nation (
+
+    nation varchar(255)
+    tags varchar(255)
+
+)
